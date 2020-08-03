35c60ab44de3422ef16f6d7b11f23561644648aa
d3q3 <projectd3q@drikus.net>
2020-08-03 22:16:14 +0200

    Deletes old files in js an ts first

--- learnOpenGL2_p7B/part7/js/common/KeyInput.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class KeyInput {
-    keysDown: any;
-    constructor(keys: any);
-    registerKeyDown(event: KeyboardEvent): void;
-    registerKeyUp(event: KeyboardEvent): void;
-    isDown(key: any): boolean;
-}
