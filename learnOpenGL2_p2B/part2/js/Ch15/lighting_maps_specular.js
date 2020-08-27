1f59d310a530f034dd6ef399cd43029caf0b25fa
d3q3 <projectd3q@drikus.net>
2020-08-27 12:35:32 +0200

    licencingJoey part2

--- learnOpenGL2_p2B/part2/js/Ch15/lighting_maps_specular.js
+++ learnOpenGL2_p2B/part2/js/Ch15/lighting_maps_specular.js
@@ -1,3 +1,7 @@
+// This code is a javascript translation of code originally written by Joey de Vries under the CC BY-NC 4.0 licence. 
+// For more information please visit https://learnopengl.com/About
+
+
 import { vs_lighting_maps, fs_lighting_maps, vs_lamp, fs_lamp } from "../../js/Ch15/shaders/index.js";
 import { vec3, mat4 } from "../../../math/glmatrix/index.js";
 import { Shader } from "../../js/common/Shader.js";
